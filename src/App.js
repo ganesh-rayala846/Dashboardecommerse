89c680f8db07038a14578d34574135abb62b7a5e
Ganesh-Rayala <rayalagamesh151@gmail.com>
2025-06-22 13:31:57 +0530

    dash

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Sidebar from './components/sidebar';
 import UserProfileMenu from './components/userprofilemenu';
 import Dashboard from './components/dashboard';
 
+
+
+
 function App() {
   return (
+
      <div className='app-container'>
         <Sidebar />
         <div className="main-content">
